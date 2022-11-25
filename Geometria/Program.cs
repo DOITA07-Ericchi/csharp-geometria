db576baba400a59c52f8697b45612285c03c32d7
EricchiYukia <git@ericchi.moe>
2022-11-25 16:05:33 +0100

    Punti 1 e 2 fatti

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Geometria;
 
+Rettangolo rettangolo = new Rettangolo();
+Console.Write("Inserisci la base: ");
+rettangolo.baseRettangolo = int.Parse(Console.ReadLine());
+Console.Write("Inserisci l'altezza: ");
+rettangolo.altezzaRettangolo = int.Parse(Console.ReadLine());
+
+int area = rettangolo.calcolaArea(rettangolo.baseRettangolo, rettangolo.altezzaRettangolo);
+int perimetro = rettangolo.calcolaPerimetro(rettangolo.baseRettangolo, rettangolo.altezzaRettangolo);
+
+Console.WriteLine($"Area: {area}\tPerimetro: {perimetro}");
+
+Console.Write("Premi Invio per uscire.");
+Console.ReadLine();
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Geometria {
+    public class Rettangolo {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        public int calcolaPerimetro(int rBase, int rAltezza) {
+            return ((rBase * 2) + (rAltezza * 2));
+        }
+        public int calcolaArea(int rBase, int rAltezza) {
+            return (rBase * rAltezza);
+        }
+        public string disegna(int rBase, int rAltezza) {
+            string k = ""
+        }
+    }
+}
+
