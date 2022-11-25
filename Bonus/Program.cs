3a9d6fa5ac67408183b94729042996e12b930256
EricchiYukia <git@ericchi.moe>
2022-11-25 16:46:03 +0100

    Bonus completato

---
+++ Bonus/Program.cs
@@ -0,0 +1,26 @@
+﻿using Geometria;
+
+Rettangolo rettangolo = new Rettangolo();
+Console.Write("Inserisci la base: ");
+rettangolo.baseRettangolo = int.Parse(Console.ReadLine());
+Console.Write("Inserisci l'altezza: ");
+rettangolo.altezzaRettangolo = int.Parse(Console.ReadLine());
+rettangolo.stampaRettangolo();
+
+
+Rettangolo rettangolo2 = new Rettangolo();
+rettangolo2.baseRettangolo = 5;
+rettangolo2.altezzaRettangolo = 6;
+
+Rettangolo rettangolo3 = new Rettangolo();
+rettangolo3.baseRettangolo = 3;
+rettangolo3.altezzaRettangolo = 7;
+
+Console.WriteLine("Eccoti altri rettangoli:");
+rettangolo2.stampaRettangolo();
+rettangolo3.stampaRettangolo();
+
+rettangolo.disegna();
+
+Console.Write("Premi Invio per uscire.");
+Console.ReadLine();
---
+++ Bonus/Rettangolo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Geometria {
+    public class Rettangolo {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        public int calcolaPerimetro() {
+            return ((baseRettangolo * 2) + (altezzaRettangolo * 2));
+        }
+        public int calcolaArea() {
+            return (baseRettangolo * altezzaRettangolo);
+        }
+        public string stampaRettangolo() {
+            int area = calcolaArea();
+            int perimetro = calcolaPerimetro();
+            string output = $"Area: {area}\tPerimetro: {perimetro}";
+            Console.WriteLine(output);
+            return output;
+        }
+        public string disegna() {
+            string k = "┌";
+            for (int i = 0; i < baseRettangolo; i++) {
+                k += "──";
+            }
+            k += "┐\n";
+            for (int i = 0; i < altezzaRettangolo; i++) {
+                k += "│";
+                for (int j = 0; j < baseRettangolo; j++) {
+                    k += "  ";
+                }
+                k += "│\n";
+            }
+            k += "└";
+            for (int i = 0; i < baseRettangolo; i++) {
+                k += "──";
+            }
+            k += "┘\n";
+            Console.Write(k);
+            return k;
+        }
+    }
+}
+
